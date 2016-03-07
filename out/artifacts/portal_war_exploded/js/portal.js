623f121291916878a5504e030d4a0cc6a7c96056
Dimon <D_Bot@ukr.net>
2016-03-07 17:26:11 +0200

    add redux.js, implement counter with redux store

--- out/artifacts/portal_war_exploded/js/portal.js
+++
@@ -1,8 +0,0 @@
-ReactDOM.render(
-    React.createElement('h1', null, 'Some react component!'),
-    document.getElementById('container')
-);
-
-define("portal.init", [], function () {
-    return "test-portal";
-});
---
+++ portal/src/main/webapp/js/reducers.js
@@ -0,0 +1,20 @@
+define([], function () {
+    return {
+        counterReducer: function (state, action) {
+            if (typeof state === 'undefined') {
+                return 0
+            }
+
+            switch (action.type) {
+                case 'INCREMENT':
+                    return state + 1;
+                case 'DECREMENT':
+                    return state - 1;
+                default:
+                    return state
+            }
+        }
+    }
+});
+
+
