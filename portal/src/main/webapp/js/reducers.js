ac0c4786a86897c0a27496f80da99c95b35c23b9
Dimon <D_Bot@ukr.net>
2016-03-08 15:40:20 +0200

    combine reducers, apply async data fetching

--- portal/src/main/webapp/js/reducers.js
+++ portal/src/main/webapp/js/reducers.js
@@ -2,18 +2,31 @@ define([], function () {
     return {
         counterReducer: function (state, action) {
             if (typeof state === 'undefined') {
-                return {counter: 0}
-            }
-
-            if (typeof state.counter === 'undefined') {
-                return state.counter = 0
+                return 0
             }
 
             switch (action.type) {
                 case 'INCREMENT':
-                    return $.extend({}, state, {counter: state.counter + 1});
+                    return state + 1;
                 case 'DECREMENT':
-                    return $.extend({}, state, {counter: state.counter - 1});
+                    return state - 1;
+                default:
+                    return state
+            }
+        },
+
+        personsReducer: function (state, action) {
+            console.log("Persons reducer: " + action);
+            if (typeof state === 'undefined') {
+                return {isFetching: false, items: []};
+            }
+
+            switch (action.type) {
+                case 'REQUEST_PERSONS_LIST':
+                    return $.extend({}, state, {isFetching: true, items: []});
+                case 'RECEIVE_PERSONS_LIST':
+                    console.log("RECEIVE_PERSONS_LIST " + action.items);
+                    return $.extend({}, state, {isFetching: false, items: action.items});
                 default:
                     return state
             }
